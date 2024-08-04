4dcf8c90cda30f43dc9f200d96c027f0a5b4eece
Julian Cataldo <contact@juliancataldo.com>
2024-08-04 18:04:44 +0200

    ci: add format to cloc script

--- scripts/code-line-counts.sh
+++ scripts/code-line-counts.sh
@@ -8,3 +8,5 @@ echo "\n# $(date '+%D') \n\n\`\`\`\n" >>code-line-counts.md &&
 	cloc --match-f=$(git ls-files packages/{gracile,engine,internal,server,client,create-gracile}/**/*.{js,ts}) \
 		--exclude-dir='integration' >>code-line-counts.md &&
 	echo '\n```\n' >>code-line-counts.md
+
+pnpm prettier --write ./code-line-counts.md
