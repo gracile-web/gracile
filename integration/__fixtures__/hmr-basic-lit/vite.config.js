4233d0f2dd2f3f19dd21b18f206b076cc5c293a8
Julian Cataldo <contact@juliancataldo.com>
2026-03-07 14:23:53 +0100

    test: clean old + fix hmr plugin path

--- integration/__fixtures__/hmr-basic-lit/vite.config.js
+++ integration/__fixtures__/hmr-basic-lit/vite.config.js
@@ -1,9 +1,9 @@
-import { hmrPlugin, presets } from '@gracile-labs/hmr';
+import { gracileHmr, presets } from '@gracile-labs/hmr';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
-		hmrPlugin({
+		gracileHmr({
 			include: ['src/**/*'],
 			presets: [presets.lit],
 		}),
